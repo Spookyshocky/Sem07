7d95b9df14ae32eb9292a0788eb4a52af20a677b
Alen <Spookyshocky@ya.ru>
2023-02-14 13:56:43 +0100

    Сделал дз под семинаром 7

---
+++ task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void GetArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("введите количество строк");
+int row = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите количество столбцов");
+int col = int.Parse(Console.ReadLine()!);
+double[,] numbers = new double[row, col];
+GetArray(numbers);
+PrintArray(numbers);
---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void GetArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} \t");
+        }   
+        
+        Console.WriteLine(""); 
+    }
+}
+
+Console.Clear();
+Console.WriteLine("введите номер строки");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите номер столбца");
+int m = int.Parse(Console.ReadLine()!);
+int [,] numbers = new int [3,4];
+GetArray(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
+}
+
+PrintArray(numbers);
